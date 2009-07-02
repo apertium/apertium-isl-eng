31f7200b581f344d5ff85ca8b0ef7e2f775be53a
Martha Dís Brandt <marthab08@ru.is>
2009-07-02 15:57:55 +0000

    adding to the bidix

--- pipelineIce.sh
+++ pipelineIce.sh
@@ -1,1 +1,1 @@
-echo "stór hundur" | ./icetaggerApertium.sh | apertium-pretransfer | apertium-transfer apertium-is-en.is-en.t1x  is-en.t1x.bin  is-en.autobil.bin  | apertium-interchunk apertium-is-en.is-en.t2x  is-en.t2x.bin
+echo $1 | ./icetaggerApertium.sh | apertium-pretransfer | apertium-transfer apertium-is-en.is-en.t1x  is-en.t1x.bin  is-en.autobil.bin  | apertium-interchunk apertium-is-en.is-en.t2x  is-en.t2x.bin | apertium-postchunk apertium-is-en.is-en.t3x is-en.t3x.bin | lt-proc -g is-en.autogen.bin | lt-proc -p is-en.autopgen.bin
