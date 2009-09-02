5992911fd11c529607742698db712dca9f095f9f
Hrafn Loftsson <hrafn@ru.is>
2009-09-02 10:58:08 +0000

    Changed icetaggerApertium.sh

--- icetaggerApertium.sh
+++ icetaggerApertium.sh
@@ -1,2 +1,2 @@
-IceNLP_Path=~/Desktop/IceNLP
-java -Xmx768M -classpath $IceNLP_Path/dist/IceNLPCore.jar:$IceNLP_Path/lib/Lemmatizer.jar is.ru.cs.nlp.icenlp.runner.RunIceTaggerApertium -tm $IceNLP_Path/dict/icetagger/otb.apertium.dict -ls $IceNLP_Path/dict/lemmald/settings.txt $1 $2 
+IceNLP_Path=~/nlp/java/IceNLP
+java -Xmx768M -classpath $IceNLP_Path/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceTaggerApertium -tm $IceNLP_Path/dict/icetagger/otb.apertium.dict -lem $1 $2 
