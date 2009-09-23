cbea83902450d7e35ef31918909ea5b6c54d14a9
Hrafn Loftsson <hrafn@ru.is>
2009-09-23 12:16:09 +0000

    added regression test shell with IceTagger

--- pipeline.sh
+++ pipeline.sh
@@ -28,12 +28,7 @@ apertium-interchunk apertium-is-en.is-en.t2x  is-en.t2x.bin |
 apertium-postchunk apertium-is-en.is-en.t3x  is-en.t3x.bin  | lt-proc -d
 is-en.autogen.bin
 
-$ echo "stór hundur" | lt-proc is-en.automorf.bin | apertium-tagger -g
-is-en.prob | apertium-pretransfer | apertium-transfer
-apertium-is-en.is-en.t1x  is-en.t1x.bin  is-en.autobil.bin  |
-apertium-interchunk apertium-is-en.is-en.t2x  is-en.t2x.bin |
-apertium-postchunk apertium-is-en.is-en.t3x  is-en.t3x.bin  | lt-proc -d
-is-en.autogen.bin | lt-proc -p is-en.autopgen.bin
+echo "stór hundur" | lt-proc is-en.automorf.bin | apertium-tagger -g is-en.prob | apertium-pretransfer | apertium-transfer apertium-is-en.is-en.t1x  is-en.t1x.bin  is-en.autobil.bin  | apertium-interchunk apertium-is-en.is-en.t2x  is-en.t2x.bin | apertium-postchunk apertium-is-en.is-en.t3x  is-en.t3x.bin  |lt-proc -d is-en.autogen.bin | lt-proc -p is-en.autopgen.bin
 
 //shortcuts:
 $ echo "Stóri hundurinn" | apertium -d . is-en-chunker
