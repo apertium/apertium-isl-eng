a8da60aee8ffb92fc849378c73b9b6dc99780999
Hrafn Loftsson <hrafn@ru.is>
2010-01-25 18:08:51 +0000

    A small change in icetaggerApertium.sh

--- icetaggerApertium.sh
+++ icetaggerApertium.sh
@@ -1,2 +1,2 @@
 IceNLP_Path=~/nlp/java/IceNLP
-java -Xmx768M -classpath $IceNLP_Path/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceTaggerApertium -tm $IceNLP_Path/dict/icetagger/otb.apertium.dict -lem $1 $2 
+java -Xmx768M -classpath $IceNLP_Path/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceTaggerApertium -tm $IceNLP_Path/dict/icetagger/otb.apertium.dict $*
