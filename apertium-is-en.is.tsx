ae90f6e2661c57a7519439be1f510dd50c3f3e08
Francis M. Tyers <ftyers@prompsit.com>
2010-02-13 13:48:19 +0000

    add tsx file

---
+++ apertium-is-en.is.tsx
@@ -0,0 +1,349 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tagger SYSTEM "../tagger.dtd">
+<tagger name="icelandic">
+<tagset>
+   <def-label name="PREP" closed="true">
+    <tags-item tags="pr"/>
+  </def-label>
+  <def-label name="PRNPERSNOM" closed="true">
+    <tags-item tags="prn.p1.*.*.nom"/>
+    <tags-item tags="prn.p2.*.*.nom"/>
+    <tags-item tags="prn.p3.*.*.nom"/>
+  </def-label>
+  <def-label name="PRNPERSACC" closed="true">
+    <tags-item tags="prn.p1.*.*.cac"/>
+    <tags-item tags="prn.p2.*.*.cac"/>
+    <tags-item tags="prn.p3.*.*.cac"/>
+  </def-label>
+  <def-label name="PRNPERSDAT" closed="true">
+    <tags-item tags="prn.p1.*.*.dat"/>
+    <tags-item tags="prn.p2.*.*.dat"/>
+    <tags-item tags="prn.p3.*.*.dat"/>
+  </def-label>
+  <def-label name="PRNPERSGEN" closed="true">
+    <tags-item tags="prn.p1.*.*.gen"/>
+    <tags-item tags="prn.p2.*.*.gen"/>
+    <tags-item tags="prn.p3.*.*.gen"/>
+  </def-label>
+  <def-label name="PRNPOS" closed="true">
+    <tags-item tags="prn.pos.*"/>
+  </def-label>
+  <def-label name="PRNREF" closed="true">
+    <tags-item tags="prn.ref.*"/>
+  </def-label>
+  <def-label name="PRN" closed="true">
+    <tags-item tags="prn.*"/>
+  </def-label> 
+  <def-label name="CNJCOO" closed="true">
+    <tags-item tags="cnjcoo"/>
+  </def-label>
+  <def-label name="CNJSUB" closed="true">
+    <tags-item tags="cnjsub"/>
+  </def-label>
+  <def-label name="DETDEM" closed="true">
+    <tags-item tags="det.dem.*"/>
+  </def-label>
+  <def-label name="DETIND" closed="true">
+    <tags-item tags="det.ind.*"/>
+  </def-label>
+  <def-label name="DETDEF" closed="true">
+    <tags-item tags="det.def.*"/>
+  </def-label> 
+  <def-label name="DETPOS" closed="true">
+    <tags-item tags="det.pos.*"/>
+  </def-label>
+  <def-label name="DETORD" closed="true">
+    <tags-item tags="det.ord.*"/>
+  </def-label>
+  <def-label name="DETQNT" closed="true">
+    <tags-item tags="det.qnt.*"/>
+  </def-label>
+  <def-label name="INTERJECCIONS">
+    <tags-item tags="ij"/>
+  </def-label>
+  <def-label name="REL" closed="true">
+    <tags-item tags="rel.*"/>
+    <tags-item tags="rel"/>
+  </def-label>
+  <def-label name="PERSONES">
+    <tags-item tags="np.ant.*"/>
+    <tags-item tags="np.cog.*"/>
+  </def-label>
+  <def-label name="NPALTRES">
+    <tags-item tags="np.al.*"/>
+    <tags-item tags="np.org.*"/>
+  </def-label>
+  <def-label name="TOPONIMS">
+    <tags-item tags="np.top.*"/>
+  </def-label>
+  <def-label name="NOMSMASCNOM">
+    <tags-item tags="n.m.*.nom.*"/>
+  </def-label>
+  <def-label name="NOMSMASCACC">
+    <tags-item tags="n.m.*.acc.*"/>
+  </def-label>
+  <def-label name="NOMSMASCDAT">
+    <tags-item tags="n.m.*.dat.*"/>
+  </def-label>
+  <def-label name="NOMSMASCGEN">
+    <tags-item tags="n.m.*.gen.*"/>
+  </def-label>
+  <def-label name="NOMSFEMNOM">
+    <tags-item tags="n.f.*.nom.*"/>
+  </def-label>
+  <def-label name="NOMSFEMACC">
+    <tags-item tags="n.f.*.acc.*"/>
+  </def-label>
+  <def-label name="NOMSFEMDAT">
+    <tags-item tags="n.f.*.dat.*"/>
+  </def-label>
+  <def-label name="NOMSFEMGEN">
+    <tags-item tags="n.f.*.gen.*"/>
+  </def-label>
+  <def-label name="NOMSNEUNOM">
+    <tags-item tags="n.nt.*.nom.*"/>
+  </def-label>
+  <def-label name="NOMSNEUACC">
+    <tags-item tags="n.nt.*.acc.*"/>
+  </def-label>
+  <def-label name="NOMSNEUDAT">
+    <tags-item tags="n.nt.*.dat.*"/>
+  </def-label>
+  <def-label name="NOMSNEUGEN">
+    <tags-item tags="n.nt.*.gen.*"/>
+  </def-label>
+  <def-label name="PREADV" closed="true">
+    <tags-item tags="preadv"/>
+  </def-label>
+  <def-label name="ADV">
+    <tags-item tags="adv"/>
+  </def-label>
+  <def-label name="ADVS">
+    <tags-item tags="adv.*"/>
+  </def-label>
+  <def-label name="CONJADV">
+    <tags-item tags="cnjadv"/>
+  </def-label>
+  <def-label name="ADJINDECL">
+    <tags-item tags="adj"/>
+  </def-label>
+  <def-label name="ADJNOMSTA">
+    <tags-item tags="adj.*.*.*.nom.sta"/>
+    <tags-item tags="adj.*.*.nom.sta"/>
+    <tags-item tags="adj.*.*.*.nom"/>
+  </def-label>
+  <def-label name="ADJNOMVEI">
+    <tags-item tags="adj.*.*.*.nom.vei"/>
+    <tags-item tags="adj.*.*.nom.vei"/>
+  </def-label>
+  <def-label name="ADJACCSTA">
+    <tags-item tags="adj.*.*.*.acc.sta"/>
+    <tags-item tags="adj.*.*.acc.sta"/>
+    <tags-item tags="adj.*.*.*.acc"/>
+  </def-label>
+  <def-label name="ADJACCVEI">
+    <tags-item tags="adj.*.*.*.acc.vei"/>
+    <tags-item tags="adj.*.*.acc.vei"/>
+  </def-label>
+  <def-label name="ADJDATSTA">
+    <tags-item tags="adj.*.*.*.dat.sta"/>
+    <tags-item tags="adj.*.*.dat.sta"/>
+    <tags-item tags="adj.*.*.*.dat"/>
+  </def-label>
+  <def-label name="ADJDATVEI">
+    <tags-item tags="adj.*.*.*.dat.vei"/>
+    <tags-item tags="adj.*.*.dat.vei"/>
+  </def-label>
+  <def-label name="ADJGENSTA">
+    <tags-item tags="adj.*.*.*.gen.sta"/>
+    <tags-item tags="adj.*.*.gen.sta"/>
+    <tags-item tags="adj.*.*.*.gen"/>
+  </def-label>
+  <def-label name="ADJGENVEI">
+    <tags-item tags="adj.*.*.*.gen.vei"/>
+    <tags-item tags="adj.*.*.gen.vei"/>
+  </def-label>
+  <def-label name="ABBR">
+    <tags-item tags="abbr"/>
+  </def-label>
+
+  <def-label name="NUM">
+    <tags-item tags="num.*"/>
+    <tags-item tags="num"/>
+  </def-label>
+
+  <def-label name="NUMSMASCNUM">
+    <tags-item tags="n.m.*.nom.*"/>
+  </def-label>
+  <def-label name="NUMSMASCACC">
+    <tags-item tags="n.m.*.acc.*"/>
+  </def-label>
+  <def-label name="NUMSMASCDAT">
+    <tags-item tags="n.m.*.dat.*"/>
+  </def-label>
+  <def-label name="NUMSMASCGEN">
+    <tags-item tags="n.m.*.gen.*"/>
+  </def-label>
+  <def-label name="NUMSFEMNUM">
+    <tags-item tags="n.f.*.nom.*"/>
+  </def-label>
+  <def-label name="NUMSFEMACC">
+    <tags-item tags="n.f.*.acc.*"/>
+  </def-label>
+  <def-label name="NUMSFEMDAT">
+    <tags-item tags="n.f.*.dat.*"/>
+  </def-label>
+  <def-label name="NUMSFEMGEN">
+    <tags-item tags="n.f.*.gen.*"/>
+  </def-label>
+  <def-label name="NUMSNEUNUM">
+    <tags-item tags="n.nt.*.nom.*"/>
+  </def-label>
+  <def-label name="NUMSNEUACC">
+    <tags-item tags="n.nt.*.acc.*"/>
+  </def-label>
+  <def-label name="NUMSNEUDAT">
+    <tags-item tags="n.nt.*.dat.*"/>
+  </def-label>
+  <def-label name="NUMSNEUGEN">
+    <tags-item tags="n.nt.*.gen.*"/>
+  </def-label>
+
+
+  <def-label name="SUPN">
+    <tags-item tags="vblex.*.supn"/>
+    <tags-item tags="vblex.supn"/>
+  </def-label>
+  <def-label name="PP">
+    <tags-item tags="vblex.*.pp.*"/>
+    <tags-item tags="vblex.pp.*"/>
+    <tags-item tags="vblex.pp"/>
+  </def-label>
+  <def-label name="PPRES">
+    <tags-item tags="vblex.*.ppres"/>
+    <tags-item tags="vblex.ppres"/>
+  </def-label>
+  <def-label name="INF">
+    <tags-item tags="vblex.*.inf"/>
+    <tags-item tags="vblex.inf"/>
+  </def-label>
+  <def-label name="VSERPPRES" closed="true">
+    <tags-item tags="vbser.ppres"/>
+  </def-label>
+  <def-label name="VSERSUPN" closed="true">
+    <tags-item tags="vbser.supn"/>
+  </def-label>
+  <def-label name="VSERPP" closed="true">
+    <tags-item tags="vbser.pp.*"/>
+    <tags-item tags="vbser.pp"/>
+  </def-label>
+  <def-label name="VSERINF" closed="true">
+    <tags-item tags="vbser.inf"/>
+  </def-label>
+  <def-label name="VSERFIN" closed="true">
+    <tags-item tags="vbser.*.pri.*"/>
+    <tags-item tags="vbser.*.past.*"/>
+    <tags-item tags="vbser.*.prs.*"/>
+    <tags-item tags="vbser.*.pss.*"/>
+    <tags-item tags="vbser.pri.*"/>
+    <tags-item tags="vbser.past.*"/>
+    <tags-item tags="vbser.prs.*"/>
+    <tags-item tags="vbser.pss.*"/>
+  </def-label>
+  <def-label name="VHAVERPPRES" closed="true">
+    <tags-item tags="vbhaver.ppres"/>
+  </def-label>
+  <def-label name="VHAVERSUPN" closed="true">
+    <tags-item tags="vbhaver.supn"/>
+  </def-label>
+  <def-label name="VHAVERINF" closed="true">
+    <tags-item tags="vbhaver.inf"/>
+  </def-label>
+  <def-label name="VHAVERPP" closed="true">
+    <tags-item tags="vbhaver.pp.*"/>
+    <tags-item tags="vbhaver.pp"/>
+  </def-label>
+  <def-label name="VHAVERFIN" closed="true">
+    <tags-item tags="vbhaver.*.pri.*"/>
+    <tags-item tags="vbhaver.*.past.*"/>
+    <tags-item tags="vbhaver.*.prs.*"/>
+    <tags-item tags="vbhaver.*.pss.*"/>
+    <tags-item tags="vbhaver.pri.*"/>
+    <tags-item tags="vbhaver.past.*"/>
+    <tags-item tags="vbhaver.prs.*"/>
+    <tags-item tags="vbhaver.pss.*"/>
+  </def-label>
+  <def-label name="VAUXPPRES" closed="true">
+    <tags-item tags="vaux.ppres"/>
+  </def-label>
+  <def-label name="VAUXSUPN" closed="true">
+    <tags-item tags="vaux.supn"/>
+  </def-label>
+  <def-label name="VAUXPP" closed="true">
+    <tags-item tags="vaux.pp.*"/>
+    <tags-item tags="vaux.pp"/>
+  </def-label>
+  <def-label name="VAUXINF" closed="true">
+    <tags-item tags="vaux.inf"/>
+  </def-label>
+  <def-label name="VAUXFIN" closed="true">
+    <tags-item tags="vaux.*.pri.*"/>
+    <tags-item tags="vaux.*.past.*"/>
+    <tags-item tags="vaux.*.prs.*"/>
+    <tags-item tags="vaux.*.pss.*"/>
+    <tags-item tags="vaux.pri.*"/>
+    <tags-item tags="vaux.past.*"/>
+    <tags-item tags="vaux.prs.*"/>
+    <tags-item tags="vaux.pss.*"/>
+  </def-label>
+  <def-label name="VFIN">
+    <tags-item tags="vblex.actv.pri.*"/>
+    <tags-item tags="vblex.actv.past.*"/>
+    <tags-item tags="vblex.actv.prs.*"/>
+    <tags-item tags="vblex.actv.pss.*"/>
+    <tags-item tags="vblex.midv.pri.*"/>
+    <tags-item tags="vblex.midv.past.*"/>
+    <tags-item tags="vblex.midv.prs.*"/>
+    <tags-item tags="vblex.midv.pss.*"/>
+    <tags-item tags="vblex.pri.*"/>
+    <tags-item tags="vblex.past.*"/>
+    <tags-item tags="vblex.prs.*"/>
+    <tags-item tags="vblex.pss.*"/>
+  </def-label>
+  <def-label name="IMP">
+    <tags-item tags="vblex.*.imp.*"/>
+    <tags-item tags="vbser.*.imp.*"/>
+    <tags-item tags="vbhaver.*.imp.*"/>
+    <tags-item tags="vaux.*.imp.*"/>
+    <tags-item tags="vblex.imp.*"/>
+    <tags-item tags="vbser.imp.*"/>
+    <tags-item tags="vbhaver.imp.*"/>
+    <tags-item tags="vaux.imp.*"/>
+  </def-label>
+
+</tagset>
+
+  <forbid>
+    <label-sequence>
+      <label-item label="DETIND"/>
+      <label-item label="PRNPERSNOM"/>
+    </label-sequence>
+    <label-sequence>
+      <label-item label="DETIND"/>
+      <label-item label="PRNPERSNOM"/>
+    </label-sequence>
+
+  </forbid>
+
+    <!--
+    <enforce-rules>
+      <enforce-after label="DETDEF">
+        <label-set>
+        <label-item label="ADVHÄR"/>
+      </label-set>	
+     </enforce-after>
+
+    </enforce-rules>
+    -->
+
+</tagger>
