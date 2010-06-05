7527af60f12c99de5676cc5216a4ced0610c60e1
Hrafn Loftsson <hrafn@ru.is>
2010-06-05 16:51:34 +0000

    Added a parameter to stop after cg has been run

--- icenlpApertium.sh
+++ icenlpApertium.sh
@@ -5,16 +5,18 @@
 #
 #  Flags:
 # -t only perform PoS tagging
+# -c run tagging and constraint grammar
 # -s run standard version of IceNLP instead of IceNLPServer
 
 # Make sure we are in this apertium directory
 cd `dirname $0`
 
 # Process command line arguments
-while getopts "ts" flag
+while getopts "tsc" flag
 do
   #echo "$flag" $OPTIND $OPTARG
   if [ "$flag" = "t" ] ; then flagOnlyTagging=1 ;  fi
+  if [ "$flag" = "c" ] ; then flagCG=1 ;  fi
   if [ "$flag" = "s" ] ; then flagStandardIce=1 ;  fi
 done
 
@@ -25,11 +27,10 @@ shift $(($OPTIND - 1))
 # Set commands to be used below
 apertiumPipeline="cg-proc is-en.rlx.bin | cg-proc -n is-en.lex.bin | apertium-pretransfer | apertium-transfer -n apertium-is-en.is-en.t0x is-en.t0x.bin | apertium-transfer apertium-is-en.is-en.t1x  is-en.t1x.bin  is-en.autobil.bin  | apertium-interchunk apertium-is-en.is-en.t2x  is-en.t2x.bin | apertium-interchunk apertium-is-en.is-en.t3x is-en.t3x.bin | apertium-postchunk apertium-is-en.is-en.t4x is-en.t4x.bin | lt-proc -g is-en.autogen.bin | lt-proc -p is-en.autopgen.bin"
 
-#apertiumPipeline="cg-proc is-en.rlx.bin"
+apertiumPipelineCG="cg-proc is-en.rlx.bin | cg-proc -n is-en.lex.bin" 
 
 standardIceNLP="java -Xmx768M -classpath $IceNLPCore/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceTaggerApertium -tm $IceNLPCore/dict/icetagger/otb.apertium.dict -sf -mt end"
 
-#clientIceNLP="java -jar $IceNLPClient/IceNLPClient.jar --host=localhost $1" 
 clientIceNLP=$IceNLPServer/sh/RunClient.sh $1
 
 # Execution
@@ -39,14 +40,24 @@ then
 	then
 		command=$standardIceNLP
 	else
-		command=$standardIceNLP"|"$apertiumPipeline
+		if [ -n "$flagCG" ]  # Is the variable defined?
+		then
+			command=$standardIceNLP"|"$apertiumPipelineCG
+		else
+			command=$standardIceNLP"|"$apertiumPipeline
+		fi
 	fi
 else
 	if [ -n "$flagOnlyTagging" ]  # Is the variable defined?
 	then
 		command=$clientIceNLP
 	else
-		command=$clientIceNLP"|"$apertiumPipeline
+		if [ -n "$flagCG" ]  # Is the variable defined?
+		then
+			command=$standardIceNLP"|"$apertiumPipelineCG
+		else
+			command=$standardIceNLP"|"$apertiumPipeline
+		fi
 	fi
 fi
 
