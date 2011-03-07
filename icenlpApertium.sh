b83f48100dc43aaa532dee01d0c03c6506aa5703
Martha Dís Brandt <marthab08@ru.is>
2011-03-07 15:02:17 +0000

    added tagfile parameter to icenlpapertium shell

--- icenlpApertium.sh
+++ icenlpApertium.sh
@@ -1,23 +1,25 @@
 # This program assumes that IceNLPServer (the demonized version of IceNLP) is already running 
 # The program relies on IceNLP to perform morphological analysis and tagging and then runs the rest of the Apertium pipeline to finish the translation process
 # The IceNLPCore envirionment variable needs to be set to the root directory of the IceNLPCore distribution
-# The IceNLPSever envirionment variable needs to be set to the root directory of the IceNLPServer distribution
+# The IceNLPServer envirionment variable needs to be set to the root directory of the IceNLPServer distribution
 #
 #  Flags:
 # -t only perform PoS tagging
 # -c run tagging and constraint grammar
 # -s run standard version of IceNLP instead of IceNLPServer
+# -f run full pipeline but output tags into tagfile as well
 
 # Make sure we are in this apertium directory
 cd `dirname $0`
 
 # Process command line arguments
-while getopts "tsc" flag
+while getopts "tscf" flag
 do
   #echo "$flag" $OPTIND $OPTARG
   if [ "$flag" = "t" ] ; then flagOnlyTagging=1 ;  fi
   if [ "$flag" = "c" ] ; then flagCG=1 ;  fi
   if [ "$flag" = "s" ] ; then flagStandardIce=1 ;  fi
+  if [ "$flag" = "f" ] ; then flagTagfile=1 ; fi
 done
 
 #  Decrements the argument pointer so it points to next argument.
@@ -34,13 +36,16 @@ standardIceNLP="java -Xmx768M -classpath $IceNLPCore/dist/IceNLPCore.jar is.iclt
 
 clientIceNLP="$IceNLPServer/sh/RunClient.sh $1 | $IceNLPServer/sh/apertium-destxt.sh"
 
+tagOutput="tee $1" ## filename for tag output is first argument after -f flag
+
+
 # Execution
-if [ -n "$flagStandardIce" ]  # Is the variable defined?
+if [ -n "$flagStandardIce" ]  # Is the standard IceNLP variable defined?
 then
-	if [ -n "$flagOnlyTagging" ]  # Is the variable defined?
+	if [ -n "$flagOnlyTagging" ]  # Is the tagging only variable defined?
 	then
 		command=$standardIceNLP
-		if [ -n "$flagCG" ]  # Is the variable defined?
+		if [ -n "$flagCG" ]  # Is the constraint grammar variable defined?
 		then
 			command= $standardIceNLP" | "$apertiumPipelineCG
 		fi
@@ -48,16 +53,23 @@ then
 		command=$standardIceNLP" | "$apertiumPipeline
 	fi
 else
-	if [ -n "$flagOnlyTagging" ]  # Is the variable defined?
+	if [ -n "$flagOnlyTagging" ]  # Is the tagging only variable defined?
 	then
 		command=$clientIceNLP
-		if [ -n "$flagCG" ]  # Is the variable defined?
+		if [ -n "$flagCG" ]  # Is the constraint grammar variable defined?
 		then
 			command=$clientIceNLP" | "$apertiumPipelineCG
 		fi
 	else
-		#command=$apertiumdeformatter" | "$clientIceNLP" | "$apertiumPipeline
-		command=$clientIceNLP" | "$apertiumPipeline
+		if [ -n "$flagTagfile" ]  # Is the tagfile variable defined?
+		then
+			#echo "Tagfile variable is set."
+			command=$clientIceNLP" | "$tagOutput" | "$apertiumPipeline
+		else
+			#echo "Tagfile variable is NOT set."
+			#command=$apertiumdeformatter" | "$clientIceNLP" | "$apertiumPipeline
+			command=$clientIceNLP" | "$apertiumPipeline
+		fi
 	fi
 fi
 
