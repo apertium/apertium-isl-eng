7898b6a8b5e7992d841c2c4b435eab63f7967db5
Tino Didriksen <mail@tinodidriksen.com>
2021-05-27 11:51:32 +0200

    OFTC, http -> https, url fixups

--- regression-tests.sh
+++ regression-tests.sh
@@ -19,8 +19,8 @@ TSTLIST=`mktemp /tmp/regression.tst.XXXXX`;
 basedir=`pwd`;
 mode=isl-eng
 
-wget -O - -q http://wiki.apertium.org/wiki/Icelandic_and_English/Regression_tests | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g' | cut -f2 -d')' | sed 's/<i>//g' | sed 's/<\/i>//g' | cut -f2 -d'*' | sed 's/→/!/g' | cut -f1 -d'!' | sed 's/(note:/!/g' | sed 's/_/ /g' | sed 's/$/../g' > $SRCLIST;
-wget -O - -q http://wiki.apertium.org/wiki/Icelandic_and_English/Regression_tests | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g' | sed 's/(\w\w)//g' | sed 's/<i>//g' | cut -f2 -d'*' | sed 's/<\/i>_→/!/g' | cut -f2 -d'!' | sed 's/_/ /g' | sed 's/^ *//g' | sed 's/ *$//g' | sed 's/$/../g' > $TRGLIST;
+wget -O - -q https://wiki.apertium.org/wiki/Icelandic_and_English/Regression_tests | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g' | cut -f2 -d')' | sed 's/<i>//g' | sed 's/<\/i>//g' | cut -f2 -d'*' | sed 's/→/!/g' | cut -f1 -d'!' | sed 's/(note:/!/g' | sed 's/_/ /g' | sed 's/$/../g' > $SRCLIST;
+wget -O - -q https://wiki.apertium.org/wiki/Icelandic_and_English/Regression_tests | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g' | sed 's/(\w\w)//g' | sed 's/<i>//g' | cut -f2 -d'*' | sed 's/<\/i>_→/!/g' | cut -f2 -d'!' | sed 's/_/ /g' | sed 's/^ *//g' | sed 's/ *$//g' | sed 's/$/../g' > $TRGLIST;
 
 apertium -d . $mode < $SRCLIST > $TSTLIST;
 
